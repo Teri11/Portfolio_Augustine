aaa39b5fdfcb70280b7527ae458f73b5909a57d7
Teri11 <augustineteri157@gmail.com>
2022-08-03 16:51:40 +0300

    Updates

---
+++ precache-manifest.1f4c4890f8d02401a4fd6f34b2527866.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8234e07cfc6fc577c48587473fd504",
+    "url": "/Portfolio_Augustine/index.html"
+  },
+  {
+    "revision": "4570615af593126161ea",
+    "url": "/Portfolio_Augustine/static/css/2.4cf68a64.chunk.css"
+  },
+  {
+    "revision": "e127ea71d8046e96df1a",
+    "url": "/Portfolio_Augustine/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "4570615af593126161ea",
+    "url": "/Portfolio_Augustine/static/js/2.b3f3c064.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio_Augustine/static/js/2.b3f3c064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e127ea71d8046e96df1a",
+    "url": "/Portfolio_Augustine/static/js/main.9f59b140.chunk.js"
+  },
+  {
+    "revision": "76d2436984e473ea58ab",
+    "url": "/Portfolio_Augustine/static/js/runtime-main.a54bde00.js"
+  }
+]);
